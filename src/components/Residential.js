eef907c6222f79bc5d0cb3ea418434ae043b4142
Amitt <amitt.bhardwj@gmail.com>
2020-12-29 13:02:10 -0500

    added signup and email

---
+++ src/components/Residential.js
@@ -0,0 +1,7 @@
+export default function Residential (){
+    return(
+        <div className="Residential">
+            <h2>How long do you need Mates for?</h2>
+        </div>
+    )
+}
---
+++ src/components/SignUp.js
@@ -0,0 +1,20 @@
+import {Link} from 'react-router-dom';
+
+function SignUp() {
+    return (
+      <div className="SignUp">
+       <h1>MoveMate</h1>
+       <h3>The hassle-free way to move anything</h3>
+       <p>We connect you with our moving partners on-demand. Click below to start booking</p>
+       <a href="#">Francais</a>
+       <input name="email" placeholder="Email" type="email"/>
+       <br/>
+       <input name="password" placeholder="password" type="password"/>
+    <button>Sign Up and Book Now</button>
+    <p>Already a User?<a href="#">Login</a></p>
+    <Link to="/email">Continue as Guest</Link>
+      </div>
+    );
+  }
+
+  export default SignUp;
